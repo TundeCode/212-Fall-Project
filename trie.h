a35c07c8f1147f9d7f38cebb5269f7989819326c
TundeCode <110928777+TundeCode@users.noreply.github.com>
2023-12-03 22:20:49 -0500

    Update trie.h
    added a deleteContactHelper and a delete contact

--- trie.h
+++ trie.h
@@ -47,6 +47,7 @@ TrieNode(){
 class Trie{
 private:
  TrieNode* root;
+     void deleteContactHelper(TrieNode* node, const std::string& contactName); 
 
    void displayContactsHelper(TrieNode* node, const std::string& prefix) {
 
@@ -82,6 +83,7 @@ private:
     void deleteNode(TrieNode* node);
     void generateDotFile(const std::string& filename);
     void displayContacts(const std::string& prefix);
+    void deleteContact(const std::string& command);
     void addContact();
 };
 
