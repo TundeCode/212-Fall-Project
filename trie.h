fa96fa63486068415d8758221ab6a2b0512a36a4
Marvens <ibmarvens@gmail.com>
2023-12-03 23:22:03 -0500

    made a mistake

--- trie.h
+++ trie.h
@@ -47,7 +47,7 @@ TrieNode(){
 class Trie{
 private:
  TrieNode* root;
-
+void deleteContactHelper(TrieNode* node, const std::string& contactName);
    void displayContactsHelper(TrieNode* node, const std::string& prefix) {
 
    
